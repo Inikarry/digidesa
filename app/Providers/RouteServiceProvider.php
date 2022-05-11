d2c3e3920327dbf737fb11bb502b6f5cf0ba5c85
Herry Timisela <herry.timisela22@gmail.com>
2022-05-11 15:09:49 +0800

    ....

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/daftar-user';
+    public const HOME = '/dashboard';
 
     /**
      * The controller namespace for the application.
